cb708543db74f23aa5ec8792a41c348dc195fcd2
kmvinoth <kmvinoth@gmail.com>
2020-09-18 11:22:42 +0200

    add poll and client id

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-central-1_XOcUwPRUo', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6h74e9gqfnfj99bkojnbi48mgp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
