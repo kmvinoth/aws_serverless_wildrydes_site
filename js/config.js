a20ebe6e142457ba9b06486face4e4aabc5059e4
kmvinoth <kmvinoth@gmail.com>
2020-09-18 11:37:42 +0200

    forgot to add region

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId:'eu-central-1_XOcUwPRUo', // e.g. us-east-2_uXboG5pAb
         userPoolClientId:'6h74e9gqfnfj99bkojnbi48mgp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
